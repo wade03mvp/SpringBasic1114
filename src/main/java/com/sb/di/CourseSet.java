0d6a84ca3d5b0414807a5ad04c169a7db87b2666
MB-teacher <MB-teacher@192.168.1.157>
2020-11-14 14:32:50 +0800

    SpringBasic

--- src/main/java/com/sb/di/CourseSet.java
+++ src/main/java/com/sb/di/CourseSet.java
@@ -3,13 +3,14 @@ package com.sb.di;
 // 套課
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class CourseSet {
     private String name; // 套課名
     private List<String> cls; // 課程內容, Ex:[Java, JavaWb, SSH, Android]
     private Set<Student> students; // 學員
-    
+    private Map<String, Integer> exams; // 考試達標分數
     public CourseSet() {
     }
 
@@ -41,11 +42,20 @@ public class CourseSet {
         this.students = students;
     }
 
-    @Override
-    public String toString() {
-        return "CourseSet{" + "name=" + name + ", cls=" + cls + ", students=" + students + '}';
+    public Map<String, Integer> getExams() {
+        return exams;
     }
 
+    public void setExams(Map<String, Integer> exams) {
+        this.exams = exams;
+    }
+
+    @Override
+    public String toString() {
+        return "CourseSet{" + "name=" + name + ", cls=" + cls + ", students=" + students + ", exams=" + exams + '}';
+    }
+
+    
     
     
 }
