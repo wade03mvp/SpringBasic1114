2848913202de65f44122352cc796990c603f5aaa
MB-teacher <MB-teacher@192.168.1.157>
2020-11-14 12:03:48 +0800

    SpringBasic

--- src/test/java/com/sb/di/Test1.java
+++ src/test/java/com/sb/di/Test1.java
@@ -7,7 +7,8 @@ public class Test1 {
     @Test
     public void t1() {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config.xml");
-        Student s1 = (Student)ctx.getBean("s1");
+        //Student s1 = (Student)ctx.getBean("s1");
+        Student s1 = ctx.getBean(Student.class);
         System.out.println(s1);
         ctx.close();
     }
