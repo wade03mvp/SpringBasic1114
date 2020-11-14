e8fc3dae0795a7a867ad16a307c8c7593ec38c76
MB-teacher <MB-teacher@192.168.1.157>
2020-11-14 13:53:04 +0800

    SpringBasic

--- src/main/java/com/sb/di/Teacher.java
+++ src/main/java/com/sb/di/Teacher.java
@@ -4,9 +4,11 @@ public class Teacher {
     private String name;
 
     public Teacher() {
+        System.out.println("老師建構子1");
     }
 
     public Teacher(String name) {
+        System.out.println("老師建構子2");
         this.name = name;
     }
 
@@ -18,6 +20,14 @@ public class Teacher {
         this.name = name;
     }
 
+    public void init() {
+        System.out.println("聘請老師");
+    }
+    
+    public void destroy() {
+        System.out.println("老師退休");
+    }
+    
     @Override
     public String toString() {
         return "Teacher{" + "name=" + name + '}';
