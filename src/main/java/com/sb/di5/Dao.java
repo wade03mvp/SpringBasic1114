18eed5974d5fb859030a4a7eb4258f8f22e0dc8d
MB-teacher <MB-teacher@192.168.1.157>
2020-11-21 15:59:59 +0800

    SpringBasic

---
+++ src/main/java/com/sb/di5/Dao.java
@@ -0,0 +1,8 @@
+package com.sb.di5;
+
+import java.util.List;
+import java.util.Map;
+
+public interface Dao {
+    List<Map<String, String>> query();
+}
---
+++ src/main/java/com/sb/di5/FundDao.java
@@ -0,0 +1,33 @@
+package com.sb.di5;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FundDao implements Dao{
+    private List<Map<String, String>> funds;
+    {
+        funds = new ArrayList<>();
+        funds.add(new HashMap<String, String>(){
+                {
+                    put("symbol", "A");
+                    put("shares", "5000");
+                }
+        });
+        funds.add(new HashMap<String, String>(){
+                {
+                    put("symbol", "B");
+                    put("shares", "4000");
+                }
+        });
+    }
+    @Override
+    public List<Map<String, String>> query() {
+        return funds;
+    }
+    
+}
