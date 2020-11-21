06f5aca2a6796a2debea051037c420ed8dc18d2a
MB-teacher <MB-teacher@192.168.1.157>
2020-11-21 16:03:03 +0800

    SpringBasic

---
+++ src/main/java/com/sb/di5/StockServiceImpl.java
@@ -0,0 +1,15 @@
+package com.sb.di5;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockServiceImpl implements StockService {
+
+    @Override
+    public List findPortfolio() {
+        // 返回所有投資組合 ?
+        return null;
+    }
+    
+}
