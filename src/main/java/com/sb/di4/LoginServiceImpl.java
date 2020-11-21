ba9aa50a9e90bcd58a405026169c1ec5270d956a
MB-teacher <MB-teacher@192.168.1.157>
2020-11-21 15:38:00 +0800

    SpringBasic

--- src/main/java/com/sb/di4/LoginServiceImpl.java
+++ src/main/java/com/sb/di4/LoginServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LoginServiceImpl implements LoginService {
     private List<Map<String, String>> users;
     {
