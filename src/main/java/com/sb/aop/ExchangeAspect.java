b21581f8f1be474057399ecab13313c2efbfad10
MB-teacher <MB-teacher@192.168.1.157>
2020-11-28 15:35:36 +0800

    SpringBasic

---
+++ src/main/java/com/sb/aop/ExchangeAspect.java
@@ -0,0 +1,26 @@
+package com.sb.aop;
+
+import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Order(3)
+public class ExchangeAspect {
+    @Around(value = "execution(* com.sb.aop.BubbleTea.*(..))")
+    public Object around(ProceedingJoinPoint joinPoint) {
+        Object result = null;
+        try {
+            result = joinPoint.proceed();
+            Integer newResult = (int)(Integer.parseInt(result + "") / 30);
+            result = newResult;
+        } catch (Throwable e) {
+        }
+        return result;
+    }
+}
