3056d47515d0ba2a240face339fa46b5190a6411
MB-teacher <MB-teacher@192.168.1.157>
2020-11-28 12:25:15 +0800

    SpringBasic

--- src/main/java/com/sb/aop/LogAspect.java
+++ src/main/java/com/sb/aop/LogAspect.java
@@ -2,6 +2,7 @@ package com.sb.aop;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +10,18 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Order(1) // 放入正整數, 數字越小越先執行 (預設是 int 的最大值)
 public class LogAspect {
+    // 切點(PointCut) + 公用表達式
+    @Pointcut(value = "execution(* com.sb.aop.*.*(..))")
+    public void cut(){}
     
     // 前置通知
-    @Before(value = "execution(public int com.sb.aop.CalcImpl.*(int, int))")
+    //@Before(value = "execution(public int com.sb.aop.CalcImpl.calc(int, int))")
+    //@Before(value = "execution(public int com.sb.aop.CalcImpl.*(int, int))")
+    //@Before(value = "execution(public int com.sb.aop.CalcImpl.*(..))")
+    //@Before(value = "execution(public int com.sb.aop.*.*(..))")
+    //@Before(value = "execution(* com.sb.aop.*.*(..))")
+    //@Before(value = "execution(* *.*(..))")
+    @Before(value = "cut()")
     public void before() {
         System.out.println("AOP 前置通知");
     }
