704c620c8424ef45eab8b2b1df2b0fc8459f6312
MB-teacher <MB-teacher@192.168.1.157>
2020-11-28 10:18:05 +0800

    SpringBasic

--- src/main/java/com/sb/proxy/sta/Main.java
+++ src/main/java/com/sb/proxy/sta/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Job proxyJob = new ProxyJob(new RealJob());
         proxyJob.work();
+        
+        Job proxyJob2 = new ProxyJob(new ProgrammerJob());
+        proxyJob2.work();
     }
+    
 }
---
+++ src/main/java/com/sb/proxy/sta/ProgrammerJob.java
@@ -0,0 +1,10 @@
+package com.sb.proxy.sta;
+
+public class ProgrammerJob implements Job {
+
+    @Override
+    public void work() {
+        System.out.println("寫 Java 程式");
+    }
+    
+}
