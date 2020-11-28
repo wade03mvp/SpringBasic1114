20ad67b4c6b1cf9bfd9f5964702ee6cc3796e26e
MB-teacher <MB-teacher@192.168.1.157>
2020-11-28 12:09:52 +0800

    SpringBasic

---
+++ src/main/java/com/sb/aop/LogAspect.java
@@ -0,0 +1,18 @@
+package com.sb.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Order(1) // 放入正整數, 數字越小越先執行 (預設是 int 的最大值)
+public class LogAspect {
+    
+    // 前置通知
+    @Before(value = "execution(public int com.sb.aop.CalcImpl.add(int, int))")
+    public void before() {
+        System.out.println("AOP 前置通知");
+    }
+}
