ac14317dc2b7024ede4edf7ad5cf99d5bcb0cc37
MB-teacher <MB-teacher@192.168.1.157>
2020-12-05 14:43:07 +0800

    SpringBasic

--- src/main/java/com/jdbc/template/tx/service/BookService.java
+++ src/main/java/com/jdbc/template/tx/service/BookService.java
@@ -3,6 +3,7 @@ package com.jdbc.template.tx.service;
 import com.jdbc.template.tx.dao.BookDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BookService {
@@ -13,8 +14,10 @@ public class BookService {
         return bookDao.getPrice(bid);
     }
     
+    @Transactional
     public void buyOne(Integer wid, Integer bid) {
         bookDao.updateStock(bid); // 修改庫存
+        int x = 10/0;
         bookDao.updateWallet(wid, bid); // 修改錢包
     }
 }
--- src/test/java/com/jdbc/template/tx/Update.java
+++ src/test/java/com/jdbc/template/tx/Update.java
@@ -6,6 +6,5 @@ public class Update extends TestTemplate {
     @Test
     public void t1() {
         bookController.buy(1, 1);
-        bookController.buy(1, 2);
     }
 }
