74b060c875625e9533e78155e60acbfc7fb6e6f4
MB-teacher <MB-teacher@192.168.1.157>
2020-12-05 14:31:52 +0800

    SpringBasic

--- src/main/java/com/jdbc/template/tx/dao/BookDao.java
+++ src/main/java/com/jdbc/template/tx/dao/BookDao.java
@@ -10,10 +10,23 @@ public class BookDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
     
+    // 查詢書價
     public Integer getPrice(Integer bid) {
         String sql = "select price from book where bid = ?";
         Object[] args = {bid};
         return jdbcTemplate.queryForObject(sql, args, Integer.class);
     }
     
+    // 修改庫存 Stock
+    public void updateStock(Integer bid) {
+        String sql = "update stock set amount = amount - 1 where bid=?";
+        jdbcTemplate.update(sql, bid);
+    }
+    
+    // 修改錢包 Wallet
+    public void updateWallet(Integer wid, Integer bid) {
+        Integer price = getPrice(bid);
+        String sql = "update wallet set money = money - ? where wid=?";
+        jdbcTemplate.update(sql, price, wid);
+    }
 }
