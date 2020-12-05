939da8ef3bd7cf6c0b10e6792ba1ba4bc1e94a29
MB-teacher <MB-teacher@192.168.1.157>
2020-12-05 14:05:34 +0800

    SpringBasic

---
+++ src/main/java/com/jdbc/tx/dao/BookDao.java
@@ -0,0 +1,19 @@
+package com.jdbc.tx.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookDao {
+    
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    
+    public int getPrice(Integer bid) {
+        String sql = "select price from book where bid = ?";
+        Object[] args = {bid};
+        return jdbcTemplate.queryForObject(sql, args, Integer.class);
+    }
+    
+}
