58376e2a17044cefd8ecdfbf8d2ccef20de90d69
MB-teacher <MB-teacher@192.168.1.157>
2020-12-05 10:40:54 +0800

    SpringBasic

--- src/test/java/com/jdbc/template/single/Create2.java
+++ src/test/java/com/jdbc/template/single/Create2.java
@@ -4,15 +4,13 @@ import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-public class Create1 {
+public class Create2 {
     @Test
-    public void t1() {
+    public void t2() {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config.xml");
         JdbcTemplate jdbcTemplate = ctx.getBean("jdbcTemplate", JdbcTemplate.class);
-        // 單筆新增資料
-        String sql = "insert into emp(name, age, sex) values(?, ?, ?)";
-        int rowCount = jdbcTemplate.update(sql, "Vincent", 18, "F");
-        System.out.println("新增異動筆數: " + rowCount);
+        // 多筆新增資料
+        
         
     }
 }
