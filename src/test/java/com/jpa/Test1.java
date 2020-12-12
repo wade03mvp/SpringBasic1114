b98dad380c0873537edd8bbfbc2605406f9ee46d
MB-teacher <MB-teacher@192.168.1.157>
2020-12-12 17:08:01 +0800

    SpringBasic

--- src/test/java/com/jpa/Test1.java
+++ src/test/java/com/jpa/Test1.java
@@ -7,7 +7,27 @@ public class Test1 extends JPATemplate {
     @Test
     public void t1() {
         //create();
-        update();
+        //update();
+        //update2();
+        update3();
+    }
+    
+    private void update3() {
+        Customer c1 = new Customer();
+        c1.setId(2);
+        c1.setLastName("Happy");
+        System.out.println(c1); // c1 是游離物件
+        session.merge(c1); // 手動更新資料
+        
+    }
+    
+    private void update2() {
+        Customer c1 = new Customer();
+        c1.setId(1);
+        c1.setLastName("JoJo");
+        System.out.println(c1); // c1 是游離物件
+        session.update(c1); // 手動更新資料
+        //c1.setLastName("Helen"); // c1 是永續物件
     }
     
     private void update() {
@@ -17,13 +37,12 @@ public class Test1 extends JPATemplate {
         System.out.println(c1);
     }
     
-    
     private void create() {
         Customer c1 = new Customer();
         c1.setLastName("Tom");
-        System.out.println(c1);
+        System.out.println(c1); // c1 是臨時物件
         session.persist(c1); // 將 customer 臨時物件存入到資料表中
-        System.out.println(c1);
+        System.out.println(c1); // c1 是永續物件
         c1.setLastName("Mary");
         System.out.println(c1);
     }
