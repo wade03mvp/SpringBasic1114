13cf4abaf2de303d7ac0592eebfb5e866ac04fb8
MB-teacher <MB-teacher@192.168.1.157>
2020-12-19 14:43:21 +0800

    SpringBasic

---
+++ src/main/java/com/jpa/entity/one2one/Boss.java
@@ -0,0 +1,48 @@
+package com.jpa.entity.one2one;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Boss")
+public class Boss {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    @Column
+    private String name;
+    
+    @OneToOne
+    private Company company;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+    
+    
+}
---
+++ src/main/java/com/jpa/entity/one2one/Company.java
@@ -0,0 +1,49 @@
+package com.jpa.entity.one2one;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Company")
+public class Company {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    @Column
+    private String name;
+    
+    @OneToOne
+    private Boss boss;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boss getBoss() {
+        return boss;
+    }
+
+    public void setBoss(Boss boss) {
+        this.boss = boss;
+    }
+    
+    
+    
+}
