209b62e9b8b3d7a7ea8f199b56c5adda4f6204e0
MB-teacher <MB-teacher@192.168.1.157>
2020-12-19 13:37:36 +0800

    SpringBasic

--- src/main/java/com/jpa/entity/twoway/Dept.java
+++ src/main/java/com/jpa/entity/twoway/Dept.java
@@ -1,6 +1,50 @@
 package com.jpa.entity.twoway;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Dept")
 public class Dept {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    @Column
+    private String name;
+    
+    @OneToMany
+    private List<Emp> emps = new ArrayList<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Emp> getEmps() {
+        return emps;
+    }
+
+    public void setEmps(List<Emp> emps) {
+        this.emps = emps;
+    }
+    
     
 }
