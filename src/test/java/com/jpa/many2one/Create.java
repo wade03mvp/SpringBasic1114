32adccbbd22f5601932cef171b5a893b6a55a2d7
MB-teacher <MB-teacher@192.168.1.157>
2020-12-19 10:32:44 +0800

    SpringBasic

--- src/test/java/com/jpa/many2one/Create.java
+++ src/test/java/com/jpa/many2one/Create.java
@@ -10,21 +10,25 @@ public class Create extends JPATemplate {
     public void t1() {
         // 新增訂單的同時也建立新客戶
         Customer c1 = new Customer();
-        c1.setLastName("John");
+        c1.setLastName("Mary");
         Orders o1 = new Orders();
-        o1.setName("Cake");
+        o1.setName("Sandwish");
         Orders o2 = new Orders();
-        o2.setName("Drink");
+        o2.setName("Coffee");
         
         // 配置關聯關係
         o1.setCustomer(c1);
         o2.setCustomer(c1);
         
         // 執行保存動作
+//        session.persist(o1);
+//        session.persist(o2);
+//        session.persist(c1);
+        // 在@ManyToOne的關聯關係中, 若先新增 one 的一方, 就不會多出 update 語句    
+        session.persist(c1);
         session.persist(o1);
         session.persist(o2);
-        session.persist(c1);
-        
+
         System.out.println("完成");
         
     }
