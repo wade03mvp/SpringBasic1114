0c9eb3522bdc15d1b43eee6615fd88278295a4e2
MB-teacher <MB-teacher@192.168.1.157>
2020-12-19 12:14:57 +0800

    SpringBasic

--- src/test/java/com/jpa/one2many/Delete.java
+++ src/test/java/com/jpa/one2many/Delete.java
@@ -10,6 +10,8 @@ public class Delete extends JPATemplate {
     @Test
     public void t1() {
         Bookshelf bookshelf = (Bookshelf)session.get(Bookshelf.class, 1);
+        // 若有設定 cascade = CascadeType.REMOVE 可以聯集刪除(多的一方也都會刪除)
+        // 若沒有設定 cascade = CascadeType.REMOVE 多的一方仍會保留資料, 只是外鍵內容 = null
         session.delete(bookshelf);
         System.out.println("完成");
         
